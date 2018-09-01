89406f751d410fa11beee999dba01cd6df7f59cc
andrewdaoust <daoust.andrew@gmail.com>
2018-09-01 17:15:34 -0400

    Added more user input to handle multiple directories of songs

--- powerhour.py
+++ powerhour.py
@@ -2,11 +2,9 @@ import csv
 from pydub import AudioSegment
 
 in_file_name = input("Enter the path or filename of the csv for generating the power hour: \n")
-# "csvs/mycsv.csv"
-song_dir = input("Enter the directory the songs are in: \n")
+song_dir = input("Enter the folder the songs are in: \n")
 out_file_name = input("Enter the name for the power hour output file (don't include the extension): \n")
 out_file_name += ".wav"
-# "mypowerhour.wav"
 
 songs = []
 with open(in_file_name) as csvfile:
@@ -20,7 +18,7 @@ transition_file = songs.pop(0)
 transition = AudioSegment.from_wav(transition_file)
 
 for item in songs:
-    filename, start_time = song_dir + item[0], item[1]
+    filename, start_time = 'songs/' + song_dir + '/' + item[0], item[1]
     song = AudioSegment.from_file(filename, format='.wav')
     start_time *= 1000
     song = song[start_time:start_time + 60000]
