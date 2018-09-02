bc2ba0e7ddebc861312aa51489be77c2c6a3f9e4
andrewdaoust <daoust.andrew@gmail.com>
2018-09-01 20:55:13 -0400

    Updates

--- powerhour.py
+++ powerhour.py
@@ -4,7 +4,7 @@ from pydub import AudioSegment
 in_file_name = input("Enter the path or filename of the csv for generating the power hour: \n")
 song_dir = input("Enter the folder the songs are in: \n")
 out_file_name = input("Enter the name for the power hour output file (don't include the extension): \n")
-out_file_name += ".wav"
+out_file_name = "out_files/" + out_file_name + ".wav"
 
 songs = []
 with open(in_file_name) as csvfile:
@@ -19,9 +19,9 @@ transition = AudioSegment.from_wav(transition_file)
 
 for item in songs:
     filename, start_time = 'songs/' + song_dir + '/' + item[0], item[1]
-    song = AudioSegment.from_file(filename, format='.wav')
+    song = AudioSegment.from_file(filename, format='wav')
     start_time *= 1000
     song = song[start_time:start_time + 60000]
     power_hour += song + transition
 
-power_hour.export(out_file_name, format=".wav")
+power_hour.export(out_file_name, format="wav")
